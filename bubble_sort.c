3ffd63551e6590529d935a09f6a8c362796809d9
chugaev <chugaev.tolya@gmail.com>
2015-11-25 18:48:16 +0300

    sorts

--- bubble_sort.c
+++ bubble_sort.c
@@ -3,34 +3,38 @@
 #include <time.h>
 
 int main(void) {
+    const int MAX_NUMBER = 100000;
     int n ;
     scanf("%d", &n);
     int* mas = (int*)malloc(sizeof(int) * n);
-
-    int i;
-    for(i = 0; i < n; i++) {
-        scanf("%d", &mas[i]);
+    if (mas == 0) {
+        printf("Memory allocation error\n");
+        return 0;
     }
-
+    clock_t start_time = clock();
+    int i;
+    for (i = 0; i < n; i++) {
+        mas[i] = rand() % MAX_NUMBER;
+    }
     int j;
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < n - i - 1; j++) {
-            if(mas[j] > mas[j + 1]) {
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (mas[j] > mas[j + 1]) {
                 int temp = mas[j];
                 mas[j] = mas[j + 1];
                 mas[j + 1] = temp;
             }
         }
     }
-
-    for(i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         printf("%d ", mas[i]);
     }
+    clock_t finish_time = clock();
 
     printf("\n");
 
-    printf("work time:%f\n", (double) clock() / CLOCKS_PER_SEC);
-	free(mas);
+    printf("work time:%f\n", (double) (finish_time - start_time) / CLOCKS_PER_SEC);
+    free(mas);
     return 0;
 }
 
--- sort_shell.c
+++ sort_shell.c
@@ -3,16 +3,28 @@
 #include <time.h>
 
 int main(void) {
+    const int MAX_NUMBER = 100000;
+    const int SIZE = 40;
     int n;
     scanf("%d", &n);
     int* mas = (int*)malloc(sizeof(int) * n);
-    int i;
-    for(i = 0; i < n; i++) {
-      scanf("%d", &mas[i]);
+    int* inc = (int*)malloc(sizeof(int) * SIZE);
+    if (inc == 0 || mas == 0) {
+        printf("Memory allocation error\n");
+        return 0;
     }
-
+    int i;
+    for (i = 0; i < n; i++) {
+        mas[i] = rand() % MAX_NUMBER;
+    }
+    /*
+     * Расчет приращений шелла методом Р. Седжвика
+     * Форумлы:
+     * d[i] = 9 * 2i - 9 * 2 ^ (i/2) + 1, если i четно
+     * d[i] = 8 * 2i - 6 * 2 ^ ((i+1) / 2) + 1, если i нечетно
+     * Источник: https://ru.wikipedia.org/wiki/Сортировка_Шелла
+     */
     int s = -1;
-    int* inc = (int*)malloc(sizeof(int) * 40);
     do {
         if (++s % 2) {
             inc[s] = 8 * (1 << s) - 6 * (1 << (s / 2)) + 1;
@@ -21,22 +33,26 @@ int main(void) {
         }
     } while( 3 * inc[s] < n);
 
+    clock_t start_time = clock();
     int j, d;
-    for(d = inc[s]; d > 0; d = inc[s--]) {
-        for(i = d; i < n; i++) {
-            for(j = i; j >= d && mas[j - d] > mas[j]; j -= d) {
+    for (d = inc[s]; d > 0; d = inc[s--]) {
+        for (i = d; i < n; i++) {
+            for (j = i; j >= d && mas[j - d] > mas[j]; j -= d) {
                 int temp = mas[j];
                 mas[j] = mas[j - d];
                 mas[j - d] = temp;
             }
         }
     }
-    for(i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         printf("%d ", mas[i]);
     }
-    printf("\n");
+    clock_t finish_time = clock();
 
-    printf("work time:%f\n", (double) clock() / CLOCKS_PER_SEC);
-	free(mas);
+    printf("\n");
+    printf("work time:%f\n", (double) (finish_time - start_time) / CLOCKS_PER_SEC);
+    free(mas);
+    free(inc);
     return 0;
 }
+
